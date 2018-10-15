38da16aee7e4b1ec76e3dc9d3435452ba913ddc1
zavanton123 <zavanton123@gmail.com>
2018-10-15 15:41:19 +0300

    Inner and nested classes

--- src/main/kotlin/com/skillcentric/Main.kt
+++ src/main/kotlin/com/skillcentric/Main.kt
@@ -1,5 +1,24 @@
 package com.skillcentric
 
+// Inner and nested classes: kotlin -> nested by default, java -> inner by default
+class MyButton {
+
+    // Nested class demo (no reference to MyButton)
+    // in java: static class NestedClass{}
+    class NestedClass
+
+    // Inner class demo (contains a reference to MyButton, i.e. this@MyButton)
+    // in java: class InnerClass{}
+    inner class InnerClass {
+
+        fun demo(): String {
+
+            // in java: MyButton.this.toString()
+            return this@MyButton.toString()
+        }
+    }
+}
+
 fun main(args: Array<String>) {
 
 
