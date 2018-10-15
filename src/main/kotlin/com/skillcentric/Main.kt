95f61a5db719073eb8380ccb2dbd0746865c5883
zavanton123 <zavanton123@gmail.com>
2018-10-15 15:49:02 +0300

    Class delegation

--- src/main/kotlin/com/skillcentric/Main.kt
+++ src/main/kotlin/com/skillcentric/Main.kt
@@ -1,5 +1,36 @@
 package com.skillcentric
 
+// Class Delegation (done manually)
+class DelegatingCollection<T> : Collection<T> {
+
+    private val innerList = arrayListOf<T>()
+
+    override val size: Int get() = innerList.size
+    override fun isEmpty(): Boolean = innerList.isEmpty()
+    override fun contains(element: T): Boolean = innerList.contains(element)
+    override fun iterator(): Iterator<T> = innerList.iterator()
+    override fun containsAll(elements: Collection<T>): Boolean = innerList.containsAll(elements)
+}
+
+// Class delegation (done using "by" keyword)
+class MyDelegatingCollection<T>(innerList: Collection<T> = ArrayList<T>()) : Collection<T> by innerList
+
+// Another delegation example
+class CountingSet<T>(val innerSet: MutableCollection<T> = HashSet<T>()) : MutableCollection<T> by innerSet {
+
+    var objectsAdded = 0
+
+    override fun add(element: T): Boolean {
+        objectsAdded++
+        return innerSet.add(element)
+    }
+
+    override fun addAll(elements: Collection<T>): Boolean {
+        objectsAdded += elements.size
+        return innerSet.addAll(elements)
+    }
+}
+
 fun main(args: Array<String>) {
 
 
