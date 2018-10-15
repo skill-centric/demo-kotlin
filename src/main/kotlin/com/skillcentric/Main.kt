a7a7886ea1d64f75954e97313419da006e45d6b8
zavanton123 <zavanton123@gmail.com>
2018-10-15 19:44:43 +0300

    Plain method removed

--- src/main/kotlin/com/skillcentric/Main.kt
+++ src/main/kotlin/com/skillcentric/Main.kt
@@ -2,35 +2,42 @@ package com.skillcentric
 
 data class Person(val name: String, val age: Int)
 
-fun findTheOldest(people: List<Person>) {
-
-    var maxAge = 0
-
-    var theOldest: Person? = null
-
-    for (person in people) {
-
-        if (person.age > maxAge) {
-            theOldest = person
-            maxAge = person.age
-        }
-    }
-
-    println(theOldest)
-
-}
-
 fun main(args: Array<String>) {
 
-    val people = listOf(Person("Alice", 29), Person("Bob", 31))
-    findTheOldest(people)
+    // Pass a lambda to a library function
+    val list = listOf(Person("Alice", 29), Person("Bob", 31))
 
-    println(people.maxBy { it.age })
+    // Example 1: how to make syntax shorter
+    list.maxBy({ person: Person -> person.age })
 
-    val sum = { numOne: Int, numTwo: Int -> numOne + numTwo}
+    // we can put the lambda outside the parenthesis
+    list.maxBy() { person: Person -> person.age }
 
+    // delete empty parenthesis
+    list.maxBy { person: Person -> person.age }
+
+    // use type inference
+    list.maxBy { person -> person.age }
+
+    // use "it" keyword
+    list.maxBy { it.age }
+
+    // Example 2:
+    list.joinToString(separator = " ", transform = { person -> person.name })
+
+    // we can put the lambda outside the parenthesis
+    list.joinToString(" ") {person -> person.name }
+
+    // Example 3: Assign lambda to a variable
+    val sum = { numOne: Int, numTwo: Int -> numOne + numTwo }
     val result = sum(1, 2)
+    println(result);
 
-    println(result)
+    // Directly call lambda
+    { println("Hello world") }()
+
+
+
+
 }
 
