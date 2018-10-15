0b8599dd305706fa2af77b3465f562cb2c9dad2e
zavanton123 <zavanton123@gmail.com>
2018-10-15 15:02:04 +0300

    Named arguments and default parameter values

--- src/main/kotlin/com/skillcentric/Main.kt
+++ src/main/kotlin/com/skillcentric/Main.kt
@@ -1,7 +1,54 @@
 package com.skillcentric
 
+// Named arguments
+// Example: val result = joinToString(someCollection, separator = " -> ", prefix = "{", postfix = "}")
+fun <T> joinToString(collection: Collection<T>,
+                     separator: String,
+                     prefix: String,
+                     postfix: String): String {
+
+    val result = StringBuilder()
+
+    result.append(prefix)
+
+    for ((index, element) in collection.withIndex()) {
+
+        if (index > 0)
+            result.append(separator)
+
+        result.append(element)
+    }
+
+    result.append(postfix)
+
+    return result.toString()
+}
+
+// Default parameter values
+fun <T> joinToStringTwo(collection: Collection<T>,
+                        separator: String = ", ",
+                        prefix: String = "[",
+                        postfix: String = "]"): String {
+
+    val result = StringBuilder()
+    result.append(prefix)
+
+    for ((index, element) in collection.withIndex()) {
+
+        if (index > 0)
+            result.append(separator)
+
+        result.append(element)
+    }
+
+    result.append(postfix)
+
+    return result.toString()
+}
+
 fun main(args: Array<String>) {
 
-
+    val names = setOf("Anton", "Galina", "Jack", "Mike")
+    println(joinToStringTwo(names))
 }
 
