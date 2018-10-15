ba8ccc520df4424b92484ff555b2b0fd03380490
zavanton123 <zavanton123@gmail.com>
2018-10-15 14:49:49 +0300

    Class declaration, value object, accessors

--- src/main/kotlin/com/skillcentric/Main.kt
+++ src/main/kotlin/com/skillcentric/Main.kt
@@ -1,7 +1,33 @@
 package com.skillcentric
 
+// Class demo (public final is default kotlin visibility)
+// this is a "value object"
+class Person(
+        val name: String,
+        var isMarried: Boolean
+)
+
+// Custom accessors
+//class Rectangle(val width: Int, val height: Int) {
+//    val isSquare: Boolean
+//        get() {
+//            return width == height
+//        }
+//
+//}
+
+class Rectangle(val width: Int, val height: Int) {
+
+    val isSquare: Boolean
+        get() = width == height
+}
+
 fun main(args: Array<String>) {
 
+    val person = Person("Jake Jameson", true)
+    println("The person is ${person.name}. Is married? - ${person.isMarried}")
 
+    val rectangle = Rectangle(10, 10)
+    println("Is the rectangle a square? - ${rectangle.isSquare}")
 }
 
