1ab6397790d3c2421df50f3fd960b09284acd8a4
zavanton123 <zavanton123@gmail.com>
2018-10-15 15:23:05 +0300

    varargs demo

--- src/main/kotlin/com/skillcentric/Main.kt
+++ src/main/kotlin/com/skillcentric/Main.kt
@@ -1,5 +1,17 @@
 package com.skillcentric
 
+// Varargs demo
+// How to call in kotlin: val result = demoList("some", *someArray)
+// * is a "spread operator", it unpacks the array elements
+fun demoList(vararg values: String): String {
+    val result = StringBuilder()
+    for (value in values) {
+        result.append(value)
+        result.append(" ")
+    }
+    return result.toString()
+}
+
 fun main(args: Array<String>) {
 
 
