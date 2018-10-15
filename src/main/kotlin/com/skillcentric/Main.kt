5829720d96dcad3656daf94b00af618c03093da7
zavanton123 <zavanton123@gmail.com>
2018-10-15 14:43:37 +0300

    Variable declarations, string template

--- src/main/kotlin/com/skillcentric/Main.kt
+++ src/main/kotlin/com/skillcentric/Main.kt
@@ -1,7 +1,30 @@
 package com.skillcentric
 
+// Variable declarations
+// immutable variable
+val firstName: String = "Anton"
+
+// type inference
+val lastName = "Zaviyalov"
+
+// mutable variable
+var age = 30
+
+fun getSecondElement(items: ArrayList<String>) = items[1]
+
+// String template
+fun demo(num: Int) {
+
+    val name = if (num % 2 == 0) "Anton" else "Galina"
+    println("Hello, $name")
+}
+
 fun main(args: Array<String>) {
 
+    val firstName = "Galina"
 
+    println(firstName)
+
+    println(lastName)
 }
 
