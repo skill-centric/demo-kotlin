da49cc531d7b9a8ed8f8a45f346b6aefd67c3287
zavanton123 <zavanton123@gmail.com>
2018-10-15 15:24:24 +0300

    Infix functions and destructuring declarations

--- src/main/kotlin/com/skillcentric/Main.kt
+++ src/main/kotlin/com/skillcentric/Main.kt
@@ -1,5 +1,25 @@
 package com.skillcentric
 
+// Infix call demo
+// Note
+// 1 to "one"
+// is the same as
+// 1.to("one")
+val map = mapOf(1 to "one", 7 to "seven", 53 to "fifty three")
+
+// "infix" modifier demo
+// how "to" function is declared
+infix fun Any.to(other: Any) = Pair(this, other)
+
+// Destructuring declaration demo
+fun demoDestructuringDeclaration(): Int {
+
+    // Destructuring declaration
+    val (number, name) = 1 to "one"
+
+    return number as Int
+}
+
 fun main(args: Array<String>) {
 
 
