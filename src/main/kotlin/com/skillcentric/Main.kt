8aaf7a31bd6a123ab8bf50c9ed0e8903eed46ae7
zavanton123 <zavanton123@gmail.com>
2018-10-15 15:37:07 +0300

    Inheritance modifiers

--- src/main/kotlin/com/skillcentric/Main.kt
+++ src/main/kotlin/com/skillcentric/Main.kt
@@ -1,7 +1,57 @@
 package com.skillcentric
 
+// Interface demo
+interface Clickable {
+
+    fun click()
+
+    // default method demo
+    fun showOff() = println("I am clickable!")
+}
+
+// Inheritance modifiers: final, open, abstract, override
+// open and final modifiers demo
+open class RichButton : Clickable {
+
+    // public final by default
+    fun disable() {
+    }
+
+    // public open for overriding in subclasses
+    open fun animate() {
+    }
+
+    // open for overriding in subclasses
+    override fun click() {
+    }
+
+//    // closed for overriding in subclasses
+//    final override fun click() {
+//
+//    }
+}
+
+
+// abstract modifier demo
+abstract class Animated {
+
+    // open by default -> must be overriden in subclass
+    abstract fun animate()
+
+    // can be overriden in subclass
+    open fun stopAnimating() {
+    }
+
+    // final by default
+    // cannot be overriden in subclass
+    fun animateTwice() {
+    }
+}
+
 fun main(args: Array<String>) {
 
+    val button = RichButton()
 
+    button.disable()
 }
 
