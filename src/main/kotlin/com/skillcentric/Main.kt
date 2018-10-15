b158a1124573a41c6cd90baa8a5bbfd8b5b05806
zavanton123 <zavanton123@gmail.com>
2018-10-15 15:42:57 +0300

    Sealed classes

--- src/main/kotlin/com/skillcentric/Main.kt
+++ src/main/kotlin/com/skillcentric/Main.kt
@@ -1,5 +1,19 @@
 package com.skillcentric
 
+// Sealed classes
+sealed class Expression {
+    class Num(val value: Int) : Expression()
+    class Sum(val left: Expression, val right: Expression) : Expression()
+}
+
+// Because we are using a sealed class
+// no "else" is required in the when clause
+fun evaluate(expression: Expression): Int =
+        when (expression) {
+            is Expression.Num -> expression.value
+            is Expression.Sum -> evaluate(expression.right) + evaluate(expression.left)
+        }
+
 fun main(args: Array<String>) {
 
 
