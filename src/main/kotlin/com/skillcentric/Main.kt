8185dfb66154dda7a13fa0b30c977ba7ba32ba00
zavanton123 <zavanton123@gmail.com>
2018-10-16 15:36:22 +0300

    Lambdas with receivers: with and apply

--- src/main/kotlin/com/skillcentric/Main.kt
+++ src/main/kotlin/com/skillcentric/Main.kt
@@ -295,7 +295,61 @@ fun createAllDoneRunnable(): Runnable {
 // the listener.
 
 
+// Lambdas with receivers: with and apply
+fun alphabet(): String {
 
+    val result = StringBuilder()
+
+    for (letter in 'A'..'Z') {
+
+        result.append(letter)
+    }
+
+    result.append("\nNow I know the alphabet!")
+    return result.toString()
+
+}
+
+fun alphabetImproved(): String {
+
+    val stringBuilder = StringBuilder()
+
+    return with(stringBuilder) {
+
+        for (letter in 'A'..'Z')
+            append(letter)
+
+        // 'this' can be used
+        this.append("Now I know")
+
+        // but 'this' can also be omitted
+        toString()
+    }
+}
+
+fun alphabetBest() = with(StringBuilder()) {
+
+    for (letter in 'A'..'Z')
+        append(letter)
+
+    // 'this' can be used
+    this.append("Now I know")
+
+    // but 'this' can also be omitted
+    toString()
+}
+
+// 'apply' demo
+// 'apply' is an extension function
+fun alphabetMore() = StringBuilder().apply {
+
+    for (letter in 'A'..'Z')
+        append(letter)
+
+    // 'this' can be used
+    this.append("Now I know")
+
+}.toString()
 
 
 
