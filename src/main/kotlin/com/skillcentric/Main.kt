474d8379052a40b7f24bfa5bd105536e89fbfde1
zavanton123 <zavanton123@gmail.com>
2018-10-17 12:59:47 +0300

    Nullable types

--- src/main/kotlin/com/skillcentric/Main.kt
+++ src/main/kotlin/com/skillcentric/Main.kt
@@ -5,3 +5,33 @@ fun main(args: Array<String>) {
 
 }
 
+fun capitalize(input: String) = input.toUpperCase()
+
+fun demoCapitalize() {
+
+    // passing null value isn't allowed by the compiler
+    capitalize(null)
+}
+
+// Nullable type: String? = String + null
+fun demo(input: String?) {
+
+    // not allowed (can use only ?. or !!. calls)
+    input.toUpperCase()
+
+    // not allowed (type mismatch)
+    val someString: String = input
+
+    // not allowed (type mismatch)
+    capitalize(input)
+
+    // you can compare with null
+    // Note: if checks are too verbose
+    if (input != null) {
+
+        // now these are allowed
+        input.length
+        val someString: String = input
+        capitalize(input)
+    }
+}
