34f4e1ee204874c68c4a5987fd3bbc69414e5304
zavanton123 <zavanton123@gmail.com>
2018-10-17 17:29:51 +0300

    Extensions on nullable types

--- src/main/kotlin/com/skillcentric/Main.kt
+++ src/main/kotlin/com/skillcentric/Main.kt
@@ -2,6 +2,30 @@ package com.skillcentric
 
 fun main(args: Array<String>) {
 
-
 }
+
+// Extensions on nullable types
+fun String?.myIsNullOrBlank(): Boolean =
+        this == null || this.isBlank()
+
+// Extension functions on nullable types can be called without safe call
+fun demoNullTypesExtension() {
+
+    val firstName: String? = "Galina"
+
+    // no safe call is needed
+    val result = firstName.myIsNullOrBlank()
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
