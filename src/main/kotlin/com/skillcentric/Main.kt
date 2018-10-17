31ca9056f105ad0efec0b1c7db06a642df9d5966
zavanton123 <zavanton123@gmail.com>
2018-10-17 17:53:22 +0300

    Nullability of type parameters

--- src/main/kotlin/com/skillcentric/Main.kt
+++ src/main/kotlin/com/skillcentric/Main.kt
@@ -5,3 +5,17 @@ fun main(args: Array<String>) {
 
 }
 
+// Nullability of type parameters
+// by default type parameters are nullable
+fun <T> printHashCode(t: T) {
+
+    // need safe case
+    println(t?.hashCode())
+}
+
+// to make the type parameter non-null use upper bound
+fun <T : Any> printHashCodeTwo(t: T) {
+
+    // needs no safe cast
+    println(t.hashCode())
+}
